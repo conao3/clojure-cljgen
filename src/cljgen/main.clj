217cc99683a0189ff9597b037c5bb09ca0462f8b
Naoya Yamashita <conao3@gmail.com>
2024-07-21 16:28:20 +0900

    spit file

--- src/cljgen/main.clj
+++ src/cljgen/main.clj
@@ -3,6 +3,7 @@
    [babashka.cli :as cli]
    [clojure.java.io :as io]
    [clojure.string :as string]
+   [clojure.tools.logging :as logging]
    [selmer.parser])
   (:gen-class))
 
@@ -30,11 +31,32 @@
        (filter #(-> (config-file-path "templates" %) .isDirectory))
        set))
 
+(defn- emit-template
+  "Emit template."
+  [template base-dir]
+  (let [template-dir (config-file-path "templates" template)
+        template-dir-path (-> template-dir .toPath)]
+    (doseq [^java.io.File template-file (file-seq template-dir)]
+      (when (-> template-file .isFile)
+        (let [template-path (-> template-file .toPath)
+              relative-path (-> template-dir-path (.relativize template-path))
+              target-file (io/file base-dir (str relative-path))
+              target-file-dir (-> target-file .getParentFile)]
+          (when-not (-> target-file-dir .isDirectory)
+            (logging/info (format "Mkdir: %s" (str target-file-dir)))
+            (-> target-file-dir .mkdirs))
+          (logging/info (format "Write: %s" (str target-file)))
+          (spit target-file (slurp template-file)))))))
+
 ;;;; Entrypoint
 
 (def cli-spec
   {:spec
-   {:template {:desc "Template name"}}})
+   {:template {:desc "Template name"}
+    :change-dir {:desc "Expand directory (Default: current-directory)"
+                 :alias :C}}
+   :exec-args
+   {:change-dir (System/getProperty "user.dir")}})
 
 (defn- get-help
   "Return help as string."
@@ -60,7 +82,7 @@
   (println (template-names))
   (println *command-line-args*)
   (let [opts (cli/parse-opts *command-line-args* cli-spec)
-        {:keys [template]} opts
+        {:keys [template change-dir]} opts
         template-candidates (template-names)]
     (when (or (:help opts) (:h opts))
       (println-err (get-help cli-spec))
@@ -71,4 +93,7 @@
       (println-err (format "%s is not defineded tempalte name.  Please specify one of %s"
                            template
                            template-candidates))
-      (System/exit 1))))
+      (System/exit 1))
+
+    (when template
+      (emit-template template change-dir))))
